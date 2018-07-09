fb3ea2d2ea05e548f0b92ba32993020fc0f5f2fc
BlixShot <umbertof93@live.it>
2018-07-09 19:51:25 +0200

    testModel

--- src/application/libretto/model/Model.java
+++ src/application/libretto/model/Model.java
@@ -1,5 +1,31 @@
 package application.libretto.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Model {
+	
+	private List<Esame> esami;
+	
+	public Model() {
+		this.esami= new ArrayList<Esame>();
+	}
+	
+	public boolean addEsame(Esame e) {
+		if(!esami.contains(e)) {
+			esami.add(e);
+			return true;
+		}
+		else
+			return false;
+	}
+	
+	public Esame trovaEsame(String codice) {
+		int pos= this.esami.indexOf(new Esame(codice, null, null));
+		if(pos==-1)
+			return null;
+		else
+			return esami.get(pos);
+	}
 
 }
